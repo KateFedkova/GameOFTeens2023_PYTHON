514bd17fddbec06ee5276e52789040fec12c0fbb
PavloShutz <pavlo050706@gmail.com>
2023-06-16 17:14:17 +0300

    Refactored bot.py. Found small errors

--- bot.py
+++ bot.py
@@ -6,10 +6,10 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.contrib.fsm_storage.redis import RedisStorage2
 
 from tgbot.config import load_config
-from tgbot.filters import AdminFilter
-from tgbot.handlers import register_admin
+from tgbot.filters.admin import AdminFilter
+from tgbot.handlers.admin import register_admin
 from tgbot.handlers.echo import register_echo
-from tgbot.handlers import register_user
+from tgbot.handlers.user import register_user
 from tgbot.middlewares.environment import EnvironmentMiddleware
 
 logger = logging.getLogger(__name__)
