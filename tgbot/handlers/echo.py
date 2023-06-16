9c2dc41f74d090f0dde088fa63683494c821f965
KateFedkova <kate.fedkova@gmail.com>
2023-06-16 19:40:47 +0300

    Add states for user

--- tgbot/handlers/echo.py
+++ tgbot/handlers/echo.py
@@ -1,6 +1,34 @@
 from aiogram import types, Dispatcher
 from aiogram.dispatcher import FSMContext
 from aiogram.utils.markdown import hcode
+from tgbot.misc import UserStates
+
+
+async def start_choosing(message: types.Message, state: FSMContext):
+    await UserStates.weeks.set()
+    await message.answer("Choose weeks")
+
+
+async def choose_weeks_quantity(message: types.Message, state: FSMContext):
+    await state.finish()
+    await UserStates.internet.set()
+    await message.answer("Choose internet quantity")
+
+
+async def choose_internet_quantity(message: types.Message, state: FSMContext):
+    await state.finish()
+    await UserStates.calls.set()
+    await message.answer("Choose calls quantity")
+
+
+async def choose_calls_quantity(message: types.Message, state: FSMContext):
+    await state.finish()
+    await UserStates.price.set()
+    await message.answer("Choose price")
+
+
+async def choose_price(message: types.Message, state: FSMContext):
+    await state.finish()
 
 
 async def bot_echo(message: types.Message):
@@ -24,5 +52,10 @@ async def bot_echo_all(message: types.Message, state: FSMContext):
 
 
 def register_echo(dp: Dispatcher):
+    dp.register_message_handler(start_choosing, commands=['choose'])
+    dp.register_message_handler(choose_weeks_quantity, state=UserStates.weeks)
+    dp.register_message_handler(choose_internet_quantity, state=UserStates.internet)
+    dp.register_message_handler(choose_calls_quantity, state=UserStates.calls)
+    dp.register_message_handler(choose_price, state=UserStates.price)
     dp.register_message_handler(bot_echo)
     dp.register_message_handler(bot_echo_all, state="*", content_types=types.ContentTypes.ANY)
---
+++ tgbot/misc/states.py
@@ -0,0 +1,8 @@
+from aiogram.dispatcher.filters.state import StatesGroup, State
+
+
+class UserStates(StatesGroup):
+    weeks = State()
+    internet = State()
+    calls = State()
+    price = State()
