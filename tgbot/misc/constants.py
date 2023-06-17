246806f3d961f88f70c5750ba45029f79d4f9254
KateFedkova <kate.fedkova@gmail.com>
2023-06-17 16:19:24 +0300

    Update constants.py

--- tgbot/misc/constants.py
+++ tgbot/misc/constants.py
@@ -20,20 +20,33 @@ DEFAULT_BOT_TEXT = """
     /choose     Обрати вигідний тариф для себе
 """
 
+RECOMMENDATION = """
+Зважаючи на ваші вподобання, пропонуємо вам тариф: 
+<b>{}</b>
 
-potuzhnyy = Tariff(weeks=4, internet=40, calls=float('inf'), price=100)
-int_bezmezh = Tariff(weeks=4, internet=float('inf'), calls=250, price=100)
-dzvinkiy = Tariff(weeks=4, internet=None, calls=float('inf'), price=100)
-vilniy_life_reg = Tariff(weeks=4, internet=float('inf'), calls=1600, price=150)
-vilniy_life = Tariff(weeks=4, internet=float('inf'), calls=1600, price=180)
-smart_life = Tariff(weeks=4, internet=25, calls=800, price=120)
-prosto_life = Tariff(weeks=4, internet=8, calls=300, price=90)
-platinum_life = Tariff(weeks=4, internet=float('inf'), calls=3000, price=250)
-shkilniy = Tariff(weeks=4, internet=7, calls=float('inf'), price=150)
-gadget_bezpeka = Tariff(weeks=12, internet=0.15, calls=15, price=90)
-gadget_smart21 = Tariff(weeks=4, internet=0.5, calls=50, price=150)
-gadget_tab21 = Tariff(weeks=4, internet=50, calls=None, price=275)
-gadget_rout21 = Tariff(weeks=4, internet=float('inf'), calls=None, price=375)
-smart_family_s = Tariff(weeks=4, internet=20, calls=500, price=375)
-smart_family_m = Tariff(weeks=4, internet=30, calls=750, price=425)
-smart_family_l = Tariff(weeks=4, internet=50, calls=1500, price=500)
+Всі деталі тарифу доступні <a href="https://www.lifecell.ua/uk/mobilnij-zvyazok/taryfy/{}/">за посиланням</a>
+
+"""
+
+potuzhnyy = Tariff(name='potuzhnyy', weeks=4, internet=40, calls=float('inf'), price=100)
+int_bezmezh = Tariff(name='int_bezmezh', weeks=4, internet=float('inf'), calls=250, price=100)
+dzvinkiy = Tariff(name='dzvinkiy', weeks=4, internet=None, calls=float('inf'), price=100)
+vilniy_life_reg = Tariff(name='vilniy_life_reg', weeks=4, internet=float('inf'), calls=1600, price=150)
+vilniy_life = Tariff(name='vilniy_life', weeks=4, internet=float('inf'), calls=1600, price=180)
+smart_life = Tariff(name='smart_life', weeks=4, internet=25, calls=800, price=120)
+prosto_life = Tariff(name='prosto_life', weeks=4, internet=8, calls=300, price=90)
+platinum_life = Tariff(name='platinum_life', weeks=4, internet=float('inf'), calls=3000, price=250)
+shkilniy = Tariff(name='shkilniy', weeks=4, internet=7, calls=float('inf'), price=150)
+gadget_bezpeka = Tariff(name='gadget_bezpeka', weeks=12, internet=0.15, calls=15, price=90)
+gadget_smart21 = Tariff(name='gadget_smart21', weeks=4, internet=0.5, calls=50, price=150)
+gadget_tab21 = Tariff(name='gadget_tab21', weeks=4, internet=50, calls=None, price=275)
+gadget_rout21 = Tariff(name='gadget_rout21', weeks=4, internet=float('inf'), calls=None, price=375)
+smart_family_s = Tariff(name='smart_family_s', weeks=4, internet=20, calls=500, price=375)
+smart_family_m = Tariff(name='smart_family_m', weeks=4, internet=30, calls=750, price=425)
+smart_family_l = Tariff(name='smart_family_l', weeks=4, internet=50, calls=1500, price=500)
+
+
+tariffs = [gadget_bezpeka, potuzhnyy, int_bezmezh, dzvinkiy, vilniy_life_reg, vilniy_life, smart_life, prosto_life,
+           platinum_life, shkilniy, gadget_smart21, gadget_tab21, gadget_rout21,
+           gadget_rout21, smart_family_s, smart_family_m, smart_family_l]
+
