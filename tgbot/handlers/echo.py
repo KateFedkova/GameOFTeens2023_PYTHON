84f4e205e3d3e65c62dae29ad509685206180f70
PavloShutz <pavlo050706@gmail.com>
2023-06-17 09:21:22 +0300

    Changed echo.py + constants.py

--- tgbot/handlers/echo.py
+++ tgbot/handlers/echo.py
@@ -1,8 +1,8 @@
 from aiogram import types, Dispatcher
 from aiogram.dispatcher import FSMContext
-from aiogram.utils.markdown import hcode
 from tgbot.misc import UserStates
 from tgbot.keyboards import weeks_keyboard, internet_keyboard, calls_keyboard
+from tgbot.misc.constants import DEFAULT_BOT_TEXT
 
 
 async def start_choosing(message: types.Message, state: FSMContext):
@@ -33,23 +33,7 @@ async def choose_price(message: types.Message, state: FSMContext):
 
 
 async def bot_echo(message: types.Message):
-    text = [
-        "Эхо без состояния.",
-        "Сообщение:",
-        message.text
-    ]
-
-    await message.answer('\n'.join(text))
-
-
-async def bot_echo_all(message: types.Message, state: FSMContext):
-    state_name = await state.get_state()
-    text = [
-        f'Эхо в состоянии {hcode(state_name)}',
-        'Содержание сообщения:',
-        hcode(message.text)
-    ]
-    await message.answer('\n'.join(text))
+    await message.answer(DEFAULT_BOT_TEXT)
 
 
 def register_echo(dp: Dispatcher):
@@ -59,4 +43,3 @@ def register_echo(dp: Dispatcher):
     dp.register_callback_query_handler(choose_calls_quantity, state=UserStates.calls, text=['calls_300', 'calls_301', 'calls_1501'])
     dp.register_callback_query_handler(choose_price, state=UserStates.price)
     dp.register_message_handler(bot_echo)
-    dp.register_message_handler(bot_echo_all, state="*", content_types=types.ContentTypes.ANY)
