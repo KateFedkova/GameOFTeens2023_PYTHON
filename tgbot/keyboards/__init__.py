b35134bd44c3110173a6c8e16b5b8506c33eb13c
PavloShutz <pavlo050706@gmail.com>
2023-06-17 07:45:09 +0300

    Refactored reply keyboards

--- tgbot/keyboards/__init__.py
+++ tgbot/keyboards/__init__.py
@@ -1,2 +1,2 @@
 from .inline import weeks_keyboard, internet_keyboard, calls_keyboard
-from .reply import startup_markup, signup_markup
+from .reply import signup_markup
--- tgbot/keyboards/reply.py
+++ tgbot/keyboards/reply.py
@@ -1,17 +1,9 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
 
-from tgbot.misc.constants import WEB_APP_URL
-
-begin_btn = KeyboardButton(text="Почати роботу")
-startup_markup = ReplyKeyboardMarkup(
-    resize_keyboard=True,
-    keyboard=[
-        [begin_btn]
-    ]
-)
+from tgbot.misc.constants import SIGNUP_PAGE
 
 web_site_btn = KeyboardButton(
     text="Тисни сюди аби зареєструватись!",
-    web_app=WebAppInfo(url=WEB_APP_URL)
+    web_app=WebAppInfo(url=SIGNUP_PAGE)
 )
 signup_markup = ReplyKeyboardMarkup(keyboard=[[web_site_btn]], resize_keyboard=True, one_time_keyboard=True)
