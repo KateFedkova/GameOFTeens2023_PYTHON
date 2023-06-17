dfec38813d3355903fc72f53c3dc4d320dd0004d
KateFedkova <kate.fedkova@gmail.com>
2023-06-17 10:58:10 +0300

    Update taryfy.py

--- tgbot/misc/taryfy.py
+++ tgbot/misc/taryfy.py
@@ -1,12 +1,13 @@
 """Class for lifecell tariffs data."""
 
 from dataclasses import dataclass
+from typing import Optional, Union
 
 
 @dataclass
 class Tariff:
 
     weeks: int
-    internet: int
-    calls: int
+    internet: Optional[Union[int, float]]
+    calls: Optional[Union[int, float]]
     price: int
